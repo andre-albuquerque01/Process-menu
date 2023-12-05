28186883b75a8ed26799c4644b09da573e93561d
andre-albuquerque01 <andrealbuquerque2001@gmail.com>
2023-12-04 21:12:22 -0300

    style: refac style

--- src/app/components/Nav/Index.tsx
+++ src/app/components/Nav/Index.tsx
@@ -3,49 +3,46 @@ import logoTake from "../../../../public/takethephone.svg"
 import iconUser from "../../../../public/userPerson.png"
 import iconCar from "../../../../public/carrinho.png"
 import iconArrowDown from "../../../../public/arrowDown.png"
-// import iconSearch from "../../../../public/search.png"
 import Link from "next/link"
 import "./style.css"
 
 export const Index = () => {
     return (
-        <div>
+        <div className="header">
             <nav>
                 <div className="image">
                     <Link href="/">
                         <Image
                             src={logoTake}
-                            width={160}
-                            height={80}
+                            className="imageTakeThePhone"
                             alt="Logo marca da empresa" />
                     </Link>
                 </div>
                 <div className="search">
                     <input type="text" name="search" id="search" />
-                    {/* <Image
-                        src={iconSearch}
-                        width={28}
-                        height={28}
-                        alt="icone de user" /> */}
                 </div>
                 <div className="list">
                     <ul>
                         <li>
                             <Image
                                 src={iconUser}
-                                width={28}
-                                height={28}
+                                width={30}
+                                height={30}
                                 alt="icone de user" />
-                            Perfil
+                            <span>
+                                Perfil
+                            </span>
                             <Image
                                 src={iconArrowDown}
-                                width={28}
-                                height={28}
-                                alt="icone da setinha" />
+                                width={30}
+                                height={30}
+                                alt="icone da setinha"
+                                title="Perfil" />
 
                             <ul className="insideList">
                                 <li>Login</li>
                                 <li>Meus pedidos</li>
+                                <li>Configuração</li>
                             </ul>
                         </li>
                         <li>
@@ -53,7 +50,8 @@ export const Index = () => {
                                 src={iconCar}
                                 width={28}
                                 height={28}
-                                alt="icone do carrinho" />
+                                alt="icone do carrinho"
+                                title="Carrinho" />
                         </li>
                     </ul>
                 </div>
